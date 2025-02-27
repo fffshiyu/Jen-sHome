8ac76b809b1d4d1dcdb48c4e7c1c41b1cc0af348
756767294@qq.com <756767294@qq.com>
2025-02-27 15:56:40 +0800

    Initial commit

---
+++ Jennie-Home/Experience/Experience.js
@@ -0,0 +1,11 @@
+import * as THREE from "three";
+export default class Experience {
+  static instance;
+  constructor(canvas) {
+    if (Experience.instance) {
+      return Experience.instance;
+    }
+    Experience.instance = this;
+    this.canvas = canvas;
+  }
+}
