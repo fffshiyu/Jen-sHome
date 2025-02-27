6daafaa3d2a1a95e3a5dcf6804325d6fc92319b1
756767294@qq.com <756767294@qq.com>
2025-02-27 16:36:47 +0800

    basic set

--- Jennie-Home/Experience/Experience.js
+++ Jennie-Home/Experience/Experience.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import Sizes from "./Utils/Sizes.js";
+import Camera from "./Utils/Camera.js";
 export default class Experience {
   static instance;
   constructor(canvas) {
@@ -7,5 +9,8 @@ export default class Experience {
     }
     Experience.instance = this;
     this.canvas = canvas;
+    this.scene = new THREE.Scene()
+    this.sizes = new Sizes()
+    this.camera = new Camera()
   }
 }
---
+++ Jennie-Home/Experience/Utils/Camera.js
@@ -0,0 +1,44 @@
+import * as THREE from "three";
+import Experience from "../Experience";
+export default class Camera{
+    constructor(){
+        this.experience=new Experience()
+        this.sizes = this.experience.sizes
+        this.scene = this.experience.scene
+        this.canvas = this.experience.canvas
+        this.createPerspectiveCamera();
+        this.createOrtherographicCamera();
+    }
+    createPerspectiveCamera(){
+        this.perspectiveCamera =new THREE.PerspectiveCamera(
+            35,
+            this.sizes.aspect,
+            0.1,
+            1000
+        )
+        this.scene.add(this.perspectiveCamera)
+    }
+    createOrtherographicCamera(){
+        this.frustrum = 5
+        this.ortherographicCamera =new THREE.ortherographicCamera(
+          (-this.sizes.aspect*this.sizes.frustrum)/2,
+          (this.sizes.aspect*this.sizes.frustrum)/2,
+            this.sizes.frustrum/2,
+            -this.sizes.frustrum/2,
+            -100,
+            100
+        
+        )
+        this.scene.add(this.ortherographicCamera)
+    }
+    resize(){
+        this.perspectiveCamera.aspect = this.sizes.aspect
+        this.perspectiveCamera.updateProjectionMatrix()
+        this.ortherographicCamera.left= (-this.sizes.aspect*this.sizes.frustrum)/2
+        this.ortherographicCamera.right= (this.sizes.aspect*this.sizes.frustrum)/2,
+        this.ortherographicCamera.top=  this.sizes.frustrum/2,
+        this.ortherographicCamera.bottom  =-this.sizes.frustrum/2,
+        this.ortherographicCamera.updateProjectionMatrix()
+
+    }
+}
